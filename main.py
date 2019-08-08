b45cd775786550ae9e63a9e41508fac003f9c1ee
Samira Abnar <samiraabnar@gmail.com>
2019-08-08 16:26:44 +0200

    read sentence wise

--- main.py
+++ main.py
@@ -102,9 +102,9 @@ else:
 
 eval_batch_size = 10
 test_batch_size = 1
-train_data = batchify(corpus.train, None, args)
-val_data = batchify(corpus.valid, None, args)
-test_data = batchify(corpus.test, None, args)
+train_data = batchify(corpus.train, eval_batch_size, args)
+val_data = batchify(corpus.valid, eval_batch_size, args)
+test_data = batchify(corpus.test, eval_batch_size, args)
 
 ###############################################################################
 # Build the model
