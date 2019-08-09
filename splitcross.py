6b30788ed9ca8b85b510a6f250f1ff884bfc0ea1
Samira Abnar <samiraabnar@gmail.com>
2019-08-09 11:17:25 +0200

    read sentence wise

--- splitcross.py
+++ splitcross.py
@@ -174,8 +174,8 @@ class SplitCrossEntropyLoss(nn.Module):
             t = length_mask.reshape(1, length_mask.size()[0] * length_mask.size()[1])
             t = t.squeeze()
             entropy = entropy * t.float()
-            print(entropy.sum)
-            print(t.sum)
+            print(entropy.sum())
+            print(t.sum())
             ###
             running_offset += len(split_hiddens[idx])
             total_loss = entropy.float().sum() if total_loss is None else total_loss + entropy.float().sum()
